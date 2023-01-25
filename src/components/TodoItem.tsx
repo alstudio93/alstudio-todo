1283e4f3a7e77493c1b2033057008d60d3cf384b
Ladd1993 <aladd1993@gmail.com>
2023-01-24 22:09:59 -0600

    update

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -3,10 +3,9 @@ import React from 'react'
 const TodoItem = () => {
     return (
         <>
-            <div className="p-2 border w-full rounded-lg ">
+            <div className="p-2 border w-full rounded-lg mb-10">
                 <h3 className="text-xl  text-slate-200 font-nunito">Todo Item 1</h3>
             </div>
-            <div className="w-[2px] bg-slate-200 h-10 my-5" />
             <span></span>
         </>
     )
