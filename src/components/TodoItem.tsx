5c5814773a7527902f62e3dc1277410ac94690dd
Ladd1993 <aladd1993@gmail.com>
2023-01-24 21:49:19 -0600

    first commit

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const TodoItem = () => {
+    return (
+        <>
+            <div className="p-2 border w-full rounded-lg ">
+                <h3 className="text-xl  text-slate-200 font-nunito">Todo Item 1</h3>
+            </div>
+            <div className="w-[2px] bg-slate-200 h-10 my-5" />
+            <span></span>
+        </>
+    )
+}
+
+export default TodoItem
---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'nunito': 'Nunito, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif',
+        'quicksand': 'Quicksand, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif'
+      },
+    },
+  },
+  plugins: [],
+};
