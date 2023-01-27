1d3b72fc0faee04a054cf02bb2423c7dcc0342aa
Ladd1993 <aladd1993@gmail.com>
2023-01-27 17:07:57 -0600

    error fix

--- src/components/CreateTodoForm.tsx
+++ src/components/CreateTodoForm.tsx
@@ -4,32 +4,34 @@ import { useForm } from "react-hook-form";
 
 
 const CreateTodoForm: React.FC<{
-    createTodoHandler: any;
+    // createTodoHandler: any;
 }> = ({
-    createTodoHandler
+    // createTodoHandler
 }) => {
         const { register, handleSubmit, watch, formState: { errors } } = useForm<CreateTodoSchema>();
         return (
-            <form onSubmit={handleSubmit(createTodoHandler)} className="grid grid-cols-2 gap-x-8 gap-y-8 justify-items-center mt-10">
+            // <form onSubmit={handleSubmit(createTodoHandler)} className="grid grid-cols-2 gap-x-8 gap-y-8 justify-items-center mt-10">
 
-                <fieldset className="flex flex-col gap-y-1">
-                    <label className="text-slate-200 text-xl" htmlFor="title">Title</label>
-                    <input {...register("title")} id="title" type="text" className="border-b-2 bg-transparent w-[300px] focus:border-b-2 focus:outline-none text-white p-2" />
-                </fieldset>
+            //     <fieldset className="flex flex-col gap-y-1">
+            //         <label className="text-slate-200 text-xl" htmlFor="title">Title</label>
+            //         <input {...register("title")} id="title" type="text" className="border-b-2 bg-transparent w-[300px] focus:border-b-2 focus:outline-none text-white p-2" />
+            //     </fieldset>
 
-                <fieldset className="flex flex-col gap-y-1">
-                    <label className="text-slate-200 text-xl" htmlFor="dueDate">Due Date</label>
-                    <input {...register("dueDate")} id="dueDate" type="date" className="border-b-2 bg-transparent w-[300px] focus:border-b-2 focus:outline-none text-white py-2" />
-                </fieldset>
+            //     <fieldset className="flex flex-col gap-y-1">
+            //         <label className="text-slate-200 text-xl" htmlFor="dueDate">Due Date</label>
+            //         <input {...register("dueDate")} id="dueDate" type="date" className="border-b-2 bg-transparent w-[300px] focus:border-b-2 focus:outline-none text-white py-2" />
+            //     </fieldset>
 
-                <fieldset className="flex flex-col gap-y-1 col-span-2 w-full ">
-                    <label className="text-slate-200 text-xl" htmlFor="optionalNote">Optional Note</label>
-                    <textarea {...register("note")} id="optionalNote" className="border bg-transparent text-white p-2 rounded-lg " />
-                </fieldset>
+            //     <fieldset className="flex flex-col gap-y-1 col-span-2 w-full ">
+            //         <label className="text-slate-200 text-xl" htmlFor="optionalNote">Optional Note</label>
+            //         <textarea {...register("note")} id="optionalNote" className="border bg-transparent text-white p-2 rounded-lg " />
+            //     </fieldset>
 
-                <button type="submit" className="border w-full p-2 text-slate-200 rounded-lg col-span-2" >Create Your TODO</button>
+            //     <button type="submit" className="border w-full p-2 text-slate-200 rounded-lg col-span-2" >Create Your TODO</button>
 
-            </form>
+            // </form>
+            <>
+            </>
         )
     }
 
