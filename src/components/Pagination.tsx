da788d607652ef1795838691ba7f01ac7565f593
Ladd1993 <aladd1993@gmail.com>
2023-01-30 13:37:38 -0600

    testing local storage

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -8,9 +8,9 @@ interface Props {
 
 const Pagination: React.FC<Props> = ({ currentPage, totalPages, setCurrentPage }) => {
     return (
-        <div className={`flex items-center justify-center  `}>
+        <div className={`flex items-center justify-center gap-x-2  w-full`}>
             <button
-                className='bg-white rounded-lg p-2 text-gray-700 mx-2'
+                className='bg-white rounded-lg p-2 text-gray-700 mx-2 w-[80px]'
                 onClick={() => setCurrentPage(currentPage - 1)}
                 disabled={currentPage === 1}
             >
@@ -18,7 +18,7 @@ const Pagination: React.FC<Props> = ({ currentPage, totalPages, setCurrentPage }
             </button>
             <span className='text-slate-200'>{currentPage} of {totalPages}</span>
             <button
-                className='bg-white rounded-lg p-2 text-gray-700 mx-2'
+                className='bg-white rounded-lg p-2 text-gray-700 mx-2 w-[80px]'
                 onClick={() => setCurrentPage(currentPage + 1)}
                 disabled={currentPage === totalPages}
             >
