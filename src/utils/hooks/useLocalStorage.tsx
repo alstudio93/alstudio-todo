ede6ce120f48c3af49b8a2d6afe11465756ade63
Ladd1993 <aladd1993@gmail.com>
2023-01-30 14:06:17 -0600

    testing local storage

--- src/utils/hooks/useLocalStorage.tsx
+++ src/utils/hooks/useLocalStorage.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-function useLocalStorageState<T>(key: string, defaultValue: T): [T, React.Dispatch<React.SetStateAction<T>>] {
+function useLocalStorageState<T>(key: string, defaultValue: boolean | []): [T, React.Dispatch<React.SetStateAction<T>>] {
     const [value, setValue] = useState<T>(() => {
         try {
             const item = window.localStorage.getItem(key);
